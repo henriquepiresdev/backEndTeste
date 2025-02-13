652b7bd481514be37607edf4120d511347c612aa
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-13 10:47:21 -0300

    feat: adicionando as dependencias do contrato na usersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
+import { PrismaUserRepository } from 'src/repository/prisma/userRepository';
+import { UserRepository } from 'src/repository/contracts/usersContracts';
+import { PrismaService } from 'prisma/prisma.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { PrismaService } from 'prisma/prisma.service';
-import { PrismaUserRepository } from 'src/repository/prisma/userRepository';
+import { Module } from '@nestjs/common';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService, PrismaUserRepository],
+  providers: [
+    UsersService,
+    PrismaService,
+    PrismaUserRepository,
+    PrismaService,
+    {
+      provide: UserRepository,
+      useClass: PrismaUserRepository,
+    },
+  ],
 })
 export class UsersModule {}
