d5c7995dc25dddd848c23a5c97ce4192e52d1850
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-13 01:05:50 -0300

    fix:atualizando repository

--- src/repository/prisma/userRepository.ts
+++ src/repository/prisma/userRepository.ts
@@ -6,12 +6,11 @@ import { PrismaService } from 'prisma/prisma.service';
 export class PrismaUserRepository {
   constructor(private prisma: PrismaService) {}
 
-  defineTransactionContext(context: Prisma.TransactionClient): void {
-    this.prisma = context as PrismaService;
-  }
-
-  removeTransactionContext(): void {
-    this.prisma = new PrismaService();
+  async create(data: Omit<User, 'id'>): Promise<User | null> {
+    const user = await this.prisma.user.create({
+      data,
+    });
+    return user;
   }
 
   async findById(id: number): Promise<User | null> {
@@ -23,14 +22,6 @@ export class PrismaUserRepository {
 
   async findAll(): Promise<User[]> {
     return await this.prisma.user.findMany();
-  }
-
-  async create(data: Omit<User, 'id'>): Promise<User | null> {
-    const user = await this.prisma.user.create({
-      data,
-    });
-
-    return user;
   }
 
   async update(id: number, data: Partial<User>): Promise<User> {
