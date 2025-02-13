1b479437c45b1ed7f9744eb437608d145ebda873
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-13 09:05:30 -0300

    feat: adicionando condiguracoes do swagger no main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Documentação de teste tecnico')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('Teste api backend')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
