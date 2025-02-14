da4ce1974d06b2194f481fb6062e014237468889
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2025-02-14 07:03:16 -0300

    feat: criando dto de response com paginacao do getAllUser

--- src/repository/contracts/usersContracts.ts
+++ src/repository/contracts/usersContracts.ts
@@ -3,8 +3,14 @@ export abstract class UserRepository {
   abstract create(
     data: Omit<User, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<User | null>;
+
   abstract getById(id: number): Promise<User | null>;
+
   abstract getAll(skip: number, take: number): Promise<User[]>;
+
   abstract update(id: number, data: Partial<User>): Promise<User>;
+
   abstract delete(id: number): Promise<void>;
+
+  abstract countUsers(): Promise<number>;
 }
